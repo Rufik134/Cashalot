89007f0894d62534b26925464839cd3a4005e2af
Rufat Abdul-zada <abdulzade.rufat@gmail.com>
2018-06-13 10:20:52 +0400

    added api

---
+++ app/src/main/java/com/corcusoft/cash_a_lot/network/request/FeedbackBody.java
@@ -0,0 +1,8 @@
+package com.corcusoft.cash_a_lot.network.request;
+
+/**
+ * Created by Rufat on 5/25/18.
+ */
+
+public class FeedbackBody {
+}
---
+++ app/src/main/java/com/corcusoft/cash_a_lot/network/response/NotificationsResponse.java
@@ -0,0 +1,8 @@
+package com.corcusoft.cash_a_lot.network.response;
+
+/**
+ * Created by Rufat on 5/29/18.
+ */
+
+public class NotificationsResponse {
+}
